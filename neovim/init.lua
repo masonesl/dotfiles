35b3a949ab1b3f767c65cbe9f9ea20b91b616d4b
Mason <mason@excalibur.lan>
2023-06-19 14:09:13 -0600

    Switch to nvchad

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,74 +1,21 @@
-require('plugins')
-require('mason').setup()
+require "core"
 
--- @TODO re-organize config directory structure
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
--- Enable line numbers
-vim.wo.number = true
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
--- Set the line numbers to relative for easy vertical jumping
-vim.wo.relativenumber = true
+require("core.utils").load_mappings()
 
-vim.g.mapleader = " "
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- Shortcut to get back to directory
-vim.keymap.set("n", "<leader>pd", vim.cmd.Ex)
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
 
--- Set tab to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-
--- Use system clipboard when used with leader key
-vim.keymap.set('n', '<leader>y', '"+yy')
-vim.keymap.set('v', '<leader>y', '"+y')
-
--- Center cursor on vertical movement
-vim.keymap.set("n", "k", "kzz")
-vim.keymap.set("n", "j", "jzz")
-
-require("tokyonight").setup({
-		style = "night",
-		transparent = true
-	})
-
-vim.cmd[[colorscheme tokyonight]]
-
-require("colorizer").setup()
-
-local colors = require("tokyonight.colors").setup()
-
-vim.api.nvim_set_hl(0, "LineNrAbove", {fg=colors.green})
-vim.api.nvim_set_hl(0, "LineNr", {fg=colors.grey})
-vim.api.nvim_set_hl(0, "LineNrBelow", {fg=colors.red})
-
--- LSP Setup
-
-local lsp = require('lsp-zero').preset({})
-
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
-end)
-
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-require('lspconfig').pyright.setup{}
-
-lsp.setup()
-
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-
-cmp.setup({
-	mapping = {
-		['<CR>'] = cmp.mapping.confirm({select = false}),
-		['<C-j>'] = cmp.mapping.select_next_item(),
-		['<C-k>'] = cmp.mapping.select_prev_item(),
-	}
-})
-
-require('lualine').setup {
-	options = {
-		theme = require('lualine.themes.auto')
-	}
-}
-
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
