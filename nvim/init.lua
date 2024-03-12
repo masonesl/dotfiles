2fe3092c3cb6270652d1459c2ae914a32dfee626
Mason <esslinger.mason@proton.me>
2024-03-11 19:49:10 -0600

    complete custom nvim transition

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,6 @@
-require('set')
-local mappings = require('mappings')
-local theme    = require('style')
-local plugins  = require('plugins')
-
-mappings.set_basic()
+require('base.set')
+require('base.remap')
+require('base.command')
 
 -- Setup the lazy plugin manager
 local lazypath = vim.fn.stdpath('data') .. 'lazy/lazy.nvim'
@@ -21,43 +18,4 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy = require('lazy')
-
--- Add the theme to the plugins array, then install our plugins
-table.insert(plugins, theme.plugin)
-lazy.setup(plugins)
-
--- Configure and load our theme
-theme.config()
-theme.load()
-
--- Load our harpoon config
-require('config.harpoon').setup()
-mappings.set_harpoon()
-
-mappings.set_telescope()
-
--- Load our treesitter config
-require('config.treesitter')
-
--- @TEMP load colorizer no config
-require('colorizer').setup()
-
-require('config.mason')
-
-require('lsp')
-mappings.set_lsp()
-
-local cmp = require('config.cmp')
-cmp.config(mappings.get_cmp())
-
-require('Comment').setup({
-  toggler = { line = '<leader>/' },
-  mappings = { basic = true },
-})
-
-require('ibl').setup({
-  indent = {
-    char = '┊',
-  },
-})
+require('lazy').setup('plugins')
