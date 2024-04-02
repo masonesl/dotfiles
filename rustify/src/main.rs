2ff2299df58797214703d05ed4f0ceb2fb608b22
Mason <esslinger.mason@proton.me>
2024-04-02 16:28:35 -0600

    use new modules

--- rustify/src/main.rs
+++ rustify/src/main.rs
@@ -1,3 +1,33 @@
+mod workspaces;
+mod args;
+
+use clap::Parser;
+
+use crate::workspaces::{listen, monitor};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli_args = args::workspaces::Args::parse();
+
+    match cli_args.command {
+        args::workspaces::Command::Active(opts) => {
+            let monitor_container = opts.monitor.get();
+
+            if opts.oneshot {
+                todo!();
+            } else {
+                listen::active(monitor_container);
+            }
+        },
+        args::workspaces::Command::Current(opts) => {
+            let monitor_name = opts.monitor.get_name();
+
+            if opts.oneshot {
+                todo!();
+            } else {
+                listen::current(monitor_name);
+            }
+        },
+    }
 }
